c065ec7f6588a994eb19c667c3d1bc63ea73ac03
GiseP06 <124733473+GiseP06@users.noreply.github.com>
2023-03-05 23:11:54 -0300

    Add files via upload

---
+++ javadistancia.js
@@ -0,0 +1,5 @@
+let distancia = prompt ("Ingrese el km de la ruta 9");
+
+let kilometro = 8; 
+
+alert ("Se encuentra a" + (distancia - kilometro) + " " + " kilometros de distancia")
---
+++ javascript.js
@@ -0,0 +1,10 @@
+let edad = prompt ("Ingrese su edad");
+
+if (edad >=18) {
+    alert ("Sos mayor de edad, podes ingresar");
+}
+    else{
+    alert ("Sos menor, entras bajo tu responsabilidad")
+
+    }
+alert ("Esta saliendo de nuestro sitio")
